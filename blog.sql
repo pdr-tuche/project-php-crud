3d9a63ea7a98a56d5b48839c019aad911d465173
victoriaflb <victoriwflb@hotmail.com>
2023-11-13 20:32:01 -0300

    MENU TAYLOR

---
+++ blog.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 09-Nov-2023 às 03:01
+-- Versão do servidor: 10.4.28-MariaDB
+-- versão do PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `theeras` varchar(100) NOT NULL,
+  `premiacoes` varchar(255) NOT NULL,
+  `albuns` varchar(100) NOT NULL,
+  `merch` varchar(255) NOT NULL,
+  `shopping` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Extraindo dados da tabela `menu`
+--
+
+INSERT INTO `menu` (`id`, `theeras`, `premiacoes`, `albuns`, `merch`, `shopping`) VALUES
+(1, '', '', '', '', '');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -1,104 +1,36 @@
-
-<?php
-    include "config.inc.php";
-
-    require_once "valida_coo.inc.php";
-        
-?>
-<!DOCTYPE html>
-<html lang="pt">
-
+<?php include "conecta.inc.php"; ?>
+<html>
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>MeuSite.com - Admin</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/modern-business.css" rel="stylesheet">
-
-    <!-- Custom Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- Custom styles for this template -->
-    <link href="bootstrap/css/" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
+    <title>Taylor Swift Tour</title>
+    <link rel="stylesheet" type="text/css" 
+          href="bootstrap/css/bootstrap.css">
+   
+	<link rel="stylesheet" href="./assets/css/menu.css">
+	<link rel="stylesheet" href="./assets/css/topo.css">
+	<link rel="icon" href="./assets/img/logo-taylor-swift.png">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/particlesjs/2.2.3/particles.min.js"></script>
+	<script src="./assets/js/menu.js"></script>
 </head>
-
 <body>
-
-    <!-- Navigation -->
-    <header class=page-header>
-        <center>
-            <h1 style="background-color: #efefef">
-                <img src="../img/logo.png">
-            </h1>
-        </center>
-    </header>
-    <!-- Page Content -->
-    <div class="container">
-
-        <!-- Page Heading/Breadcrumbs -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Painel Admin </h1>
-                <ol class="breadcrumb">
-                    <li><a href="index.php">Home</a>
-                    </li>
-                    <li class="active"><a href="?pg=listar">Listar Páginas</a></li>
-                    <li class="active"><a href="?pg=inserir">Inserir Nova Página</a></li>
-                </ol>
-            </div>
-        </div>
-        <!-- /.row -->
-        <hr>
-        <!-- Content Row -->
-        <?php
-        // Declarando as variáveis de acesso as páginas
-        $var = "admin/home.php";
-        // o @ suprime avisos de erro do PHP.
-        @$pg = $_GET['pg'];
-    
-        // Utilizando a variável Superglobal SERVER
-        // Para testar se o parâmetro Query_String está
-        // sendo utilizado.
-    
-        if(empty($_SERVER["QUERY_STRING"])) {
-            
-        } else {
-            include("$pg.php");
-        }
-    ?>
-        
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
+	<?php
+	include_once('./menu.php');
+	?>
+	
+	<div class='container'>
+	<?php
+	# área de conteúdo
+	if(empty($_SERVER["QUERY_STRING"])){
+	        $var = "";
+	        include_once("$var");
+	}else{
+	        $pg = $_GET['pg'];
+	        include_once("$pg.php");
+	}
+	?>
+	</div>
+	<?php
+		include_once('./rodape.php');
+	?>
 </body>
-
 </html>
-<?php
-//}else{
-    header("Location:login.php");
-//}
-
-?>
